6886b2fd4b9ebea2e4bcbe4a1316af2fec98894c
Bogdan <bogdan.pettrovic@gmail.com>
2025-01-01 12:52:19 +0100

    Introduced TailwindCSS

---
+++ client/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: '#35496D',
+        secondary: '#2D3B48',
+        green: '#046A1D',
+        blue: '#3E80FC',
+        pink: '#CB1BAB'     
+      },
+    },
+  },
+  plugins: [],
+}
+
