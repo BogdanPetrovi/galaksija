91dd83efe1a0f0140aed78325b1bde7c854b5787
Bogdan Petrovic <160347824+BogdanPetrovi@users.noreply.github.com>
2025-01-29 10:38:27 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const mqttUrl = process.env.MQTTURL;
 const client = mqtt.connect(mqttUrl, options);
 
 app.get('/water-the-plant', (req,res) => {
-  const plant = req.params.plant;
+  const plant = req.query.plant;
   client.publish('test', plant)
   res.status(200).send(`Upsensno ste zalili biljku "${plant}"`)
 })
